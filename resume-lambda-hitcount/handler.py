c2111e6ad4ba793817043e1569ff6860b4fff686
Edward Niescior <edniescior@gmail.com>
2024-02-06 11:55:35 -0500

    initial commit

---
+++ resume-lambda-hitcount/handler.py
@@ -0,0 +1,56 @@
+import json
+import logging
+import os
+from datetime import datetime, timezone
+from decimal import Decimal
+
+import boto3
+
+log_level = os.getenv("LOG_LEVEL", "INFO")
+
+logger = logging.getLogger()
+logger.setLevel(log_level)
+
+_dynamodb = None
+
+def get_dynamodb() -> boto3.resource:
+    global _dynamodb
+    if _dynamodb is None:
+        _dynamodb = boto3.resource("dynamodb")
+    return _dynamodb
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+def increment_count() -> int:
+    """Increment the hit count"""
+
+    # Increment the hit count of the path key in the DynamoDB table
+    dynamodb = get_dynamodb()
+    table = dynamodb.Table(os.getenv("HITS_TABLE_NAME", "NoTableConfigured"))
+    response = table.update_item(
+        Key={"path": "hit_count"},
+        UpdateExpression="ADD h_count :inc",
+        ExpressionAttributeValues={":inc": 1},
+        ReturnValues="UPDATED_NEW",
+    )
+    logger.debug(f"Response: {json.dumps(response, indent=2, cls=DecimalEncoder)}")
+    count = int(response["Attributes"]["h_count"])
+    logger.info(f"Hit count updated: {count}")
+    return count
+
+
+def lambda_handler(event, context):
+    logger.info(f"Event: {json.dumps(event, indent=2)}")
+
+    # Increment the hit count
+    count = increment_count()
+
+    return {
+        "statusCode": 200,
+        "body": f"Hit count update to: {count}",
+    }
---
+++ tests/unit/test_handler.py
@@ -0,0 +1,95 @@
+import os
+
+import boto3
+import moto
+import pytest
+from handler import increment_count, lambda_handler
+
+
+# ===============================================================
+# Fixtures
+# ===============================================================
+@pytest.fixture(scope="session")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"  # noqa
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"  # noqa
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"  # noqa
+    os.environ["AWS_SESSION_TOKEN"] = "testing"  # noqa
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+@pytest.fixture(scope="function")
+def dynamodb_client(aws_credentials):
+    """Mocked DynamoDB client."""
+    with moto.mock_dynamodb():
+        yield boto3.client("dynamodb", region_name="us-east-1")
+
+
+@pytest.fixture(scope="function")
+def dynamodb_table(dynamodb_client):
+    os.environ["HITS_TABLE_NAME"] = "hits"
+    dynamodb_client.create_table(
+        TableName="hits",
+        KeySchema=[
+            {"AttributeName": "path", "KeyType": "HASH"},
+        ],
+        AttributeDefinitions=[
+            {"AttributeName": "path", "AttributeType": "S"},
+        ],
+        ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
+    )
+    yield dynamodb_client
+    dynamodb_client.delete_table(TableName="hits")
+    del os.environ["HITS_TABLE_NAME"]
+
+
+# ===============================================================
+# Tests
+# ===============================================================
+def test_increment_counter_zero(dynamodb_table):
+    """
+    Test that the counter is incremented to 1 when it is not initialized.
+    """
+    count = increment_count()
+    assert count == 1
+
+    response = dynamodb_table.get_item(
+        TableName="hits", Key={"path": {"S": "hit_count"}}
+    )
+    assert response["Item"]["h_count"]["N"] == "1"
+
+
+def test_increment_counter_notzero(dynamodb_table):
+    """
+    Test that the counter is incremented to 1 more than the current setting.
+    """
+     # initialize the hit counter to 7
+    dynamodb_table.put_item(
+        TableName="hits",
+        Item={
+            "path": {"S": "hit_count"},
+            "h_count": {"N": "7"},
+            "h_date": {"S": "2021-01-01"},
+            "h_time": {"S": "12:00:00"},
+            "h_ip": {"S": "127.0.0.1"},
+        }
+    )
+
+    count = increment_count()
+    assert count == 8
+
+    response = dynamodb_table.get_item(
+        TableName="hits", Key={"path": {"S": "hit_count"}}
+    )
+    assert response["Item"]["h_count"]["N"] == "8"
+
+
+
+def test_lambda_handler_zero(dynamodb_table):
+    # call the lambda handler function
+    response = lambda_handler({}, {})
+    # assert that the response is a success
+    assert response["statusCode"] == 200
+    # assert that the response contains the expected message
+    assert response["body"] == "Hit count update to: 1"
